8f3e6b003bad73c21ab81bb6e9878e03eb682c6c
Attila Orosz <atttila.orosz@gmail.com>
2017-11-17 17:57:34 +0100

    Removed obsolate function from Route

--- src/Route.php
+++ src/Route.php
@@ -22,21 +22,4 @@ class Route
         return $this->{$key} !== null;
     }
 
-    /**
-     * Parse controller string and save controller class and controller action to route properties
-     *
-     * @param string $raw
-     * @throws \Error
-     */
-    public function setController(string $raw)
-    {
-        if (strpos('@', $raw) === 0) {
-            throw new \Error('Give a valid controller and action!');
-        }
-
-        $controllerParams = explode('@', $raw);
-        $this->controller = $controllerParams[0];
-        $this->action = $controllerParams[1];
-    }
-
 }
